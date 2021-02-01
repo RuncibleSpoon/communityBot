d596612aacfb9cb02904f9edc354f50ad2936d45
RuncibleSpoon <robert.haynes@gmail.com>
2021-02-01 14:16:55 -0800

    added  a -1 day thing

--- apitest.py
+++ apitest.py
@@ -1,11 +1,17 @@
 from psaw import PushshiftAPI
+from datetime import datetime, timedelta
+
+
+days_to_subtract = 1
+
+d = datetime.today() - timedelta(days=days_to_subtract)
 
 api = PushshiftAPI()
 subred = 'paloaltonetworks'
 query_term = 'prisma'
 
 # connect and query
-gen = api.search_submissions(subreddit=subred, q=query_term)
+gen = api.search_submissions(after=d, subreddit=subred, q=query_term,filter=['url','author', 'title', 'subreddit'], )
 #results = list(gen)
 
 for subm in gen:
