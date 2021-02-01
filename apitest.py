ac707c994e4856088573e3d878d199e27de596ca
RuncibleSpoon <robert.haynes@gmail.com>
2021-02-01 13:50:53 -0800

    added query and print title and link

--- apitest.py
+++ apitest.py
@@ -3,6 +3,8 @@ from psaw import PushshiftAPI
 api = PushshiftAPI()
 subred = 'checkmarx'
 query_term = 'lounge'
+
+# connect and query
 gen = api.search_submissions(subreddit=subred, q=query_term)
 #results = list(gen)
 
