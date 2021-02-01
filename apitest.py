d85c1c376ef4eeab52ec3ae571b8a420d1db74e7
RuncibleSpoon <robert.haynes@gmail.com>
2021-02-01 14:34:03 -0800

    date filter testing more date shit

--- apitest.py
+++ apitest.py
@@ -5,12 +5,13 @@ from datetime import datetime, timedelta
 days_to_subtract = 1
 
 d = datetime.today() - timedelta(days=days_to_subtract)
+bf = int(d.timestamp())
 
 api = PushshiftAPI()
 subred = 'paloaltonetworks'
 query_term = 'prisma'
 
-print('After date =',d)
+print('After date =',d,'   ',bf)
 
 # connect and query
 gen = api.search_submissions(subreddit=subred, q=query_term,filter=['full_link','author', 'title', 'subreddit'])
