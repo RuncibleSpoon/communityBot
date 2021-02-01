fdd35cf3db31096adca0dbf3b495a67aefdc94a3
RuncibleSpoon <robert.haynes@gmail.com>
2021-02-01 13:45:32 -0800

    added query and print title and link

--- apitest.py
+++ apitest.py
@@ -6,6 +6,6 @@ subred = 'checkmarx'
 gen = api.search_submissions(subreddit=subred, q='lounge')
 #results = list(gen)
 
-for subm in gen
+for subm in gen:
      print(subm.title)
      print(subm.full_link)
